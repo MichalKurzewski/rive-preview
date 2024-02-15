67f33a320928ee56311674f1b2d933e33be65775
Michal Kurzewski <kurzewski.m@gmail.com>
2024-02-15 21:38:30 +0000

    improved animation and state machine selection

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ function App() {
     "https://cdn.rive.app/animations/vehicles.riv"
   );
   const [stateMachineNames, setStateMachineNames] = useState<string[]>([]);
+  const [animationNames, setAnimationNames] = useState<string[]>([]);
   const [stateMachineName, setStateMachineName] = useState<string>("");
+  const [animationName, setAnimationName] = useState<string>("");
+  const [color, setColor] = useState<string>("#000");
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
     if (event.target.files && event.target.files.length > 0) {
@@ -21,17 +24,46 @@ function App() {
   ) => {
     setStateMachineName(event.target.value);
   };
+  const handleAnimationChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setAnimationName(event.target.value);
+  };
+  const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setColor(event.target.value);
+  };
 
   return (
-    <div className="container m-auto h-svh">
-      <form className="p-4 border-b-2 flex items-center" action="submit">
-        <input type="file" name="file" onChange={handleFileChange} />
-        <div className="border-2 w-52 p-2">
+    <div className="m-auto h-svh" style={{ background: `${color}` }}>
+      <form
+        className="p-4 items-center flex gap-5 bg-slate-500/50"
+        action="submit"
+      >
+        <div className="w-full h-full bento-box flex flex-col items-center justify-center">
+          <h1>Rive File</h1>
+          <input
+            className="h-full bento-box "
+            type="file"
+            name="file"
+            onChange={handleFileChange}
+            accept=".riv"
+          />
+        </div>
+        <div className="flex flex-col bento-box w-full h-full items-center">
+          <h1>Background color</h1>
+          <input
+            className="w-full h-20 border-2 rounded-xl"
+            type="color"
+            onChange={handleColorChange}
+          />
+        </div>
+        <div className="w-full h-full bento-box flex flex-col items-center ">
+          <h1>State Machine</h1>
           {stateMachineNames?.length > 0 && (
             <select
               value={stateMachineName}
               onChange={handleStateMachineChange}
-              className="ml-4"
+              className="w-full h-full bento-box"
             >
               <option value="">Select a State Machine</option>
               {stateMachineNames.map((name, index) => (
@@ -42,16 +74,35 @@ function App() {
             </select>
           )}
         </div>
+        <div className="w-full h-full bento-box  flex flex-col items-center ">
+          <h1>Animation</h1>
+          {stateMachineNames?.length > 0 && (
+            <select
+              value={animationName}
+              onChange={handleAnimationChange}
+              className="w-full h-full bento-box"
+            >
+              <option value="">Select Animation</option>
+              {animationNames.map((name, index) => (
+                <option key={index} value={name}>
+                  {name}
+                </option>
+              ))}
+            </select>
+          )}
+        </div>
       </form>
 
       <div
-        key={riveSrc + stateMachineName}
-        className="w-full h-[calc(100%-150px)]"
+        key={riveSrc + stateMachineName + animationName}
+        className="w-full h-[calc(100%-200px)] border-2"
       >
         <RiveCanvas
+          animation={animationName}
           src={riveSrc}
           stateMachineName={stateMachineName}
           setStateMachineNames={setStateMachineNames}
+          setAnimationNames={setAnimationNames}
         />
       </div>
     </div>
--- src/Rive/RiveCanvas.tsx
+++ src/Rive/RiveCanvas.tsx
@@ -1,24 +1,30 @@
 import { useRive } from "@rive-app/react-canvas";
 import { Dispatch, SetStateAction, useEffect } from "react";
 interface IRiveCanvasProps {
+  animation: string;
   src: string;
   stateMachineName: string;
   setStateMachineNames: Dispatch<SetStateAction<string[]>>;
+  setAnimationNames: Dispatch<SetStateAction<string[]>>;
 }
+
 const RiveCanvas: React.FC<IRiveCanvasProps> = ({
+  animation,
   src,
   stateMachineName,
   setStateMachineNames,
+  setAnimationNames,
 }) => {
   const { rive, RiveComponent } = useRive({
     src: src,
-    stateMachines: stateMachineName,
+    stateMachines: stateMachineName ? stateMachineName : animation,
+    animations: animation,
     autoplay: true,
   });
   useEffect(() => {
     if (rive?.stateMachineNames) setStateMachineNames(rive?.stateMachineNames);
-  }, [rive, setStateMachineNames]);
-
+    if (rive?.animationNames) setAnimationNames(rive?.animationNames);
+  }, [rive, setStateMachineNames, setAnimationNames]);
   return <RiveComponent />;
 };
 
